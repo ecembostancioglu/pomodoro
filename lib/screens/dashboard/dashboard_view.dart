800cb07130ffa3a235062ca3729a813de620b64e
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-05-28 02:52:15 +0300

    Changed Auth System

--- lib/screens/dashboard/dashboard_view.dart
+++ lib/screens/dashboard/dashboard_view.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_app/auth/auth_screen.dart';
 import 'package:pomodoro_app/constants/color_constants.dart';
+import 'package:provider/provider.dart';
 
-class Dashboard extends StatelessWidget {
+import '../services/auth.dart';
+
+class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
 
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+
+    Future<void> _signOut()async{
+      try{
+        Provider.of<Auth>(context,listen: false).signOutFromGoogle();
+        Provider.of<Auth>(context,listen: false).signOut();
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context)=>AuthScreen()));
+      }catch(e){
+        print(e);
+      }
+    }
   @override
   Widget build(BuildContext context) {
     Size size=MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: _signOut,
+            child: Icon(Icons.logout)),
+      ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
